a26a08b12a00f3382d38666b71de1712fb4513fc
Klaus Weinbauer <klaus@becks.bioinf.uni-leipzig.de>
2024-04-15 14:38:47 +0200

    fix linting

--- test/test_utils.py
+++ test/test_utils.py
@@ -37,6 +37,7 @@ def test_add_implicit_hydrogens_3():
     _assert_Hs(graph, 1, 3)
     _assert_Hs(graph, 4, 1)
 
+
 def test_sulfur_ring():
     graph = parse("C:1N:C:S:C:1")
     graph = add_implicit_hydrogens(graph)
@@ -46,6 +47,7 @@ def test_sulfur_ring():
     _assert_Hs(graph, 2, 1)
     _assert_Hs(graph, 3, 0)
     _assert_Hs(graph, 4, 1)
+
 
 def test_nitrogen_5ring():
     graph = parse("C:1C:N(H):C:C:1")
@@ -69,4 +71,3 @@ def test_nitrogen_6ring():
     _assert_Hs(graph, 3, 0)
     _assert_Hs(graph, 4, 1)
     _assert_Hs(graph, 5, 1)
-
