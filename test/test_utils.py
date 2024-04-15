ef2547c17f82edf03fda2a0fa924d56c9c2de123
Klaus Weinbauer <51136324+klausweinbauer@users.noreply.github.com>
2024-04-15 14:43:41 +0200

    v0.0.8 (#3)
    * Update README.md
    
    * Update test-and-lint.yml
    
    * prepare release v0.0.7
    
    * fix issue in implicit hydrogen computation
    
    * allow non-carbon atoms to belong to no functional group
    
    * fix linting
    
    * prepare release v0.0.8
    
    ---------
    
    Co-authored-by: Klaus Weinbauer <klaus@becks.bioinf.uni-leipzig.de>

--- test/test_utils.py
+++ test/test_utils.py
@@ -36,3 +36,38 @@ def test_add_implicit_hydrogens_3():
     assert 6 == len(graph)
     _assert_Hs(graph, 1, 3)
     _assert_Hs(graph, 4, 1)
+
+
+def test_sulfur_ring():
+    graph = parse("C:1N:C:S:C:1")
+    graph = add_implicit_hydrogens(graph)
+    assert 8 == len(graph)
+    _assert_Hs(graph, 0, 1)
+    _assert_Hs(graph, 1, 0)
+    _assert_Hs(graph, 2, 1)
+    _assert_Hs(graph, 3, 0)
+    _assert_Hs(graph, 4, 1)
+
+
+def test_nitrogen_5ring():
+    graph = parse("C:1C:N(H):C:C:1")
+    graph = add_implicit_hydrogens(graph)
+    assert 10 == len(graph)
+    _assert_Hs(graph, 0, 1)
+    _assert_Hs(graph, 1, 1)
+    _assert_Hs(graph, 2, 1)
+    _assert_Hs(graph, 3, 0)
+    _assert_Hs(graph, 4, 1)
+    _assert_Hs(graph, 5, 1)
+
+
+def test_nitrogen_6ring():
+    graph = parse("C:1C:C:N:C:C:1")
+    graph = add_implicit_hydrogens(graph)
+    assert 11 == len(graph)
+    _assert_Hs(graph, 0, 1)
+    _assert_Hs(graph, 1, 1)
+    _assert_Hs(graph, 2, 1)
+    _assert_Hs(graph, 3, 0)
+    _assert_Hs(graph, 4, 1)
+    _assert_Hs(graph, 5, 1)
